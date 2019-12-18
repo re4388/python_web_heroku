765066838d2c40cff2242d859c721e84f93c22c5
gitadmin <gitadmin@muenai.com>
2019-12-18 10:37:25 +0800

    m

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, request, abort
-import os 
-from boto.s3.connection import S3Connection
-
-
+import os
 
 app = Flask(__name__)
 
-var1 = S3Connection(os.environ['Access_Token'])
 
 
 @app.route("/")
 def hello():
-    return ('hello', var1)
+    return ('hello')
 
 
 
 if __name__ == "__main__":
     app.run(port=5002)
-
 
 
